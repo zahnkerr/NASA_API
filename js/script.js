e59dfa5f6a7a0b71ca4c5bc697186d800358e168
Kerry Zahn <zahnkerr@Tristans-MBP.localdomain>
2017-10-06 17:49:57 -0600

    add neos to draw function

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var apiKey = "QE8J7VpAtS0jGD6GJcPQglaLebNpXNCqX6hijaMX";
 var startDate = "2017-10-04";
 var endDate = "2017-10-05";
 var url = "https://api.nasa.gov/neo/rest/v1/feed\?"
+var neos = [];
+var json, neoCount;
 
 url += "start_date=" + startDate;
 url += "&end_date=" + endDate;
@@ -17,16 +19,31 @@ console.log(url);
 // })
 
 function preload() {
-  $.getJSON({
-    url: url,
-    success: function(json) {
-      console.log(json);
-    }
-  })
+  json = loadJSON(url);
 }
 
 function setup() {
-  createCanvas(100, 100);
-  background(color(150));
+  for(var i in json.near_earth_objects) {
+    for(var k in json.near_earth_objects[i]) {
+      append(neos, json.near_earth_objects[i][k]);
+    }
+  }
+  neoCount = neos.length;
+  createCanvas(window.innerWidth, window.innerHeight);
 
+  background(color(150));
+  console.log(json);
+  console.log(neos);
+
+}
+
+function draw() {
+  var time = millis()/2000;
+  background("white");
+  translate(width/2, height/2);
+  ellipse(0, 0, 60, 60);
+  for(var i in neos) {
+    ellipse(cos(time) * (neoCount * i), sin(time) * (neoCount * i), 5);
+    console.log(neoCount * i);
+  }
 }

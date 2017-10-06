8f5b160ef1f7d2e2b238342c6d984da9b5852551
Kerry Zahn <zahnkerr@Simplys-MacBook-Pro.local>
2017-10-05 19:52:28 -0600

    adding ajax

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var apiKey = "QE8J7VpAtS0jGD6GJcPQglaLebNpXNCqX6hijaMX";
+var startDate = "2017-10-04";
+var endDate = "2017-10-05";
+var url = "https://api.nasa.gov/neo/rest/v1/feed\?"
+
+url += "start_date=" + startDate;
+url += "&end_date=" + endDate;
+url += "&api_key=" + apiKey;
+
+console.log(url);
+
+$.getJSON({
+  url: url,
+  success: function(json) {
+    console.log(json);
+  }
+})
